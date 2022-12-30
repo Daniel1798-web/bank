39af69628326c0a090f92c3fbb17f24af19bb619
Daniel Oto Diaz <danieloto212@gmail.com>
2022-12-30 05:33:35 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -37,7 +37,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getFirestore();
+const auth = getAuth(app);
 
 export { auth, db };
